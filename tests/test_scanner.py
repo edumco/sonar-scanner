fde1c27d4055da79b466f28ed235b8f4124e11d0
edumco <edu.pombos@hotmail.com>
2020-05-20 02:40:04 -0300

    Adds test planning

---
+++ tests/test_scanner.py
@@ -0,0 +1,7 @@
+def test_all():
+    # 1. Run sonar server container
+    # 2. run sonnar-scanner container with sonar server info
+    # 3. acesss sonnar server
+    # 4. read project results
+    # 5. compare with previous version
+    pass
